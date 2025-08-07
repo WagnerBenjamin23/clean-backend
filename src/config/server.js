3ff62fe384773a06eff3b2201b41606e6b6090b0
WagnerBenjamin23 <wagnerbenjamin23@gmail.com>
2025-08-07 16:15:08 -0300

    reestructuracion y configuracion de server

---
+++ src/config/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server{
+    
+    constructor(){
+        this.port = process.env.PORT || 5000;
+        this.app = express();
+        this.middleware();
+        this.routers();   
+    }
+    
+
+    
+    middleware(){
+        this.app.use(cors());
+
+    }
+
+    routers(){
+        this.app.get('/', (req, res) => {
+            res.send('Hello World!');
+          })
+        
+          this.app.use('/api/v1', require('../routes/routes'));
+          
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Escuchando en el puerto ${this.port}`);
+          })
+    }
+}
+
+
+module.exports = Server;
