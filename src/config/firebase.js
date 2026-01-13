346af03178b91c82e2248f142b66ba3acb511bd3
WagnerBenjamin23 <wagnerbenjamin23@gmail.com>
2026-01-13 10:20:21 -0300

    fix

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,13 @@
 const admin = require ("firebase-admin");
 
-const serviceAccount = require("../../firebase-service-account.json");
+//const serviceAccount = require("../../firebase-service-account.json");
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error('FIREBASE_SERVICE_ACCOUNT no está definida');
+}
 
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 admin.initializeApp({
 
