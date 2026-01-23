965fb65af3ff3034eeff747a905f41fbb1ae39e4
WagnerBenjamin23 <wagnerbenjamin23@gmail.com>
2026-01-23 10:23:16 -0300

    middleware

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -16,31 +16,31 @@ router.get('/test', verifyToken, (req, res) => {
   res.json({ message: "Ruta test funcionando" });
 });
 
-router.post('/register', authController.registerUser);
+router.post('/register', verifyToken, authController.registerUser);
 router.post('/login', authController.loginUser);
 router.get('/roles', rolesController.testConnection);
 
 router.get('/categories', categoriesController.getAllCategories)
 
-router.get('/dashboard/metrics',  dashboardController.getDashboardStats);
+router.get('/dashboard/metrics', verifyToken, dashboardController.getDashboardStats);
 
-router.post('/categories', categoriesController.saveCategory);
+router.post('/categories',verifyToken, categoriesController.saveCategory);
 router.get('/categories', categoriesController.getAllCategories)
-router.delete('/categories/:id', categoriesController.deleteCategory);
-router.put('/categories/:id', categoriesController.updateCategory);
+router.delete('/categories/:id', verifyToken, categoriesController.deleteCategory);
+router.put('/categories/:id', verifyToken, categoriesController.updateCategory);
 
-router.post('/products', upload.array('images'), productsController.createProduct);
+router.post('/products', verifyToken,upload.array('images'), productsController.createProduct);
 router.get('/products', productsController.getProducts);
-router.patch('/products/:id/visibility', productsController.changeVisibility);
-router.put('/products/:id', productsController.updateProduct);
-router.delete('/products/:id', productsController.deleteProduct);
-router.post('/products/:id/images', productsController.uploadProductImages);
+router.patch('/products/:id/visibility', verifyToken, productsController.changeVisibility);
+router.put('/products/:id', verifyToken, productsController.updateProduct);
+router.delete('/products/:id', verifyToken, productsController.deleteProduct);
+router.post('/products/:id/images', verifyToken, productsController.uploadProductImages);
 router.get('/products/latest', productsController.getLatestProducts);
 
-router.post('/combos',  combosController.createCombo);
+router.post('/combos', verifyToken,  combosController.createCombo);
 router.get('/combos',  combosController.getCombos);
-router.put('/combos/:id', combosController.updateCombos);
-router.delete('/combos/:id', combosController.deleteCombo);
+router.put('/combos/:id', verifyToken,combosController.updateCombos);
+router.delete('/combos/:id', verifyToken, combosController.deleteCombo);
 
 
 module.exports = router;
